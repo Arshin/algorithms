cda93e8fb4707f4cb9202e2a50c3970d1b5f74c2
Arash Ashtiani <arshin.ashtiani@gmail.com>
2019-12-17 18:09:19 -0800

    bunch more algorithms

--- uniqueChar.py
+++ uniqueChar.py
@@ -1,21 +1,20 @@
 class Solution:
-    def func(self, array):
-        if len(array) < 1:
-            return peint("too small, size matters!")
-        dic = dict()
-        max_freq = 0
-        frequent = None
-        for elem in array:
-            if elem not in dic:
-                dic[elem] = 1
-            else:
-                dic[elem]+=1
-            if dic[elem] > max_freq:
-                max_freq = dic[elem]
-                frequent = elem
+    def func(self, s):
+        s = s.replace(' ', '').lower()
 
-        return frequent
+        dic = dict()
+        for char in s:
+            if char not in dic:
+                dic[char] = 1
+            else:
+                dic[char] += 1
+
+        # for char in s:
+        #     if dic[char] == 1:
+        #         return char
+
+        return (sorted(dic.items(), key= lambda x:x[1]))
 
 
 tmp = Solution()
-print(tmp.func([0,0,2,2,3,4,1,1,1,1,4,5,6,6,7,7]))
+print(tmp.func("Arash"))
